42a350db63fe03d74e4ba0d6c847b83ad40e2803
Kyle Valdillez <kyle.valdillez@gmail.com>
2017-07-19 08:11:25 -0700

    Added navigation to site with routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,41 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
 import { AppComponent }  from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
 import { HeaderComponent } from './header/header.component';
-import { ContentComponent } from './content/content.component';
+import { HomeComponent } from './home/home.component';
+import { OurStoryComponent } from './our-story/our-story.component';
+import { PhotosComponent } from './photos/photos.component';
+import { WeddingComponent } from './wedding/wedding.component';
+import { GiftsComponent } from './gifts/gifts.component';
+import { ContactComponent } from './contact/contact.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'our-story', component: OurStoryComponent },
+  { path: 'photos', component: PhotosComponent },
+  { path: 'wedding', component: WeddingComponent },
+  { path: 'gifts', component: GiftsComponent },
+  { path: 'contact', component: ContactComponent }
+]
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule // <-- import the FormsModule before binding with [(ngModel)]
+    FormsModule, // <-- import the FormsModule before binding with [(ngModel)]
+    RouterModule.forRoot(appRoutes)
   ],
   declarations: [
     AppComponent,
     HeaderComponent,
-    ServerComponent,
-    ServersComponent,
-    ContentComponent
+    HomeComponent,
+    OurStoryComponent,
+    PhotosComponent,
+    WeddingComponent,
+    GiftsComponent,
+    ContactComponent
   ],
   providers: [],
   bootstrap: [ AppComponent ]
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+	serverId: number = 10;
+	serverStatus: string = "offline";
+	serverName = "Test server";
+	userName = "";
+	allowNewServer = false;
+	serverCreationStatus = 'no server created';
+	serverCreated = false;
+	servers = ['Testserver', 'Testserver 2'];
+
+  constructor() {
+  	setTimeout(() => {
+  		this.allowNewServer = true;
+  	},2000);
+  	this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
+  ngOnInit() {
+  }
+
+  getServerStatus(){
+		return this.serverStatus;
+	}
+
+getColor(){
+	return this.serverStatus === "online" ? "green" : "red";
+	}
+
+	onCreateServer(){
+	this.serverCreated = true;
+	this.servers.push(this.serverName);
+		this.serverCreationStatus = 'server was created! Name is ' + this.serverName;
+	}
+
+	onUpdateServerName(event: Event){
+		this.serverName = (<HTMLInputElement>event.target).value;
+	}
+}
