1935d49867b61af0827bac796b4e1ce8047c6129
Kyle Valdillez <kyle.valdillez@gmail.com>
2017-08-03 15:36:29 -0700

    Working on RSVP functionality and database connection

--- src/app/rsvp/rsvp.component.ts
+++ src/app/rsvp/rsvp.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpModule, Response } from '@angular/http';
 
 import { RsvpService } from './rsvp.service';
 
@@ -11,16 +11,18 @@ import { RsvpService } from './rsvp.service';
   styleUrls: ['../app.component.css','./rsvp.component.css']
 })
 export class RsvpComponent implements OnInit {
-  @ViewChild('f') signupForm: NgForm;
-  @ViewChild('fs') mealForm: NgForm;
-  defaultSelection = "chicken";
-  user = {
-    firstName: '',
-    lastName: '',
-    mealSelection: ''
-  }
-  submitted = false;
+  @ViewChild('f') findRsvpForm: NgForm;
+  @ViewChild('fs') submitRsvpForm: NgForm;
 
+  user = {
+    firstName: "",
+    lastName: "",
+    mealSelection: ""
+  }
+  guests = [];
+  submitted = false;
+  responded = false;
+  foundRsvp = false;
   constructor(private router: Router, private rsvpService: RsvpService) { }
 
   ngOnInit() {
@@ -33,26 +35,41 @@ export class RsvpComponent implements OnInit {
     this.router.navigate(['/photos']);
   }
 
-  displayMealSelection(){
-    this.submitted = true;
-    this.user.firstName = this.signupForm.value.firstName;
-    this.user.lastName = this.signupForm.value.lastName;
-    console.log(this.signupForm);
-    this.signupForm.reset();
-  }
-
   onSubmit(){
-    this.submitted = true;
-    this.user.mealSelection = this.mealForm.value.mealSelection;
-    console.log(this.mealForm);
-    this.checkRsvp(this.user);
-    this.mealForm.reset();
+    var index = 0;
+    for (const guest of this.guests){
+      guest.rsvp = this.submitRsvpForm.value[index].rsvpSelection;
+      console.log(this.submitRsvpForm.value.rsvpSelection);
+      guest.mealSelection = this.submitRsvpForm.value[index].mealSelection;
+      index++;
+    }
+    console.log(this.guests);
+    console.log(this.submitRsvpForm.value);
+    //this.submitRsvpForm.reset();
   }
 
-  checkRsvp(user){
-    this.rsvpService.storeRsvp(user)
+  checkRsvp(){
+    this.user.firstName = this.findRsvpForm.value.firstName;
+    this.user.lastName = this.findRsvpForm.value.lastName;
+    console.log(this.findRsvpForm);
+    this.rsvpService.getRsvp(this.user.firstName,this.user.lastName)
       .subscribe(
-        (response) => console.log(response),
+        (response) => {
+          if(response != null){
+            this.guests = response.guests;
+            this.foundRsvp = true;
+            console.log(response.responded);
+            if(response.responded == "No"){
+              this.responded = false;
+
+            } else {
+              this.responded = true;
+            }
+          } else {
+            this.foundRsvp = false;
+          }
+          this.submitted = true;
+        },
         (error) => console.log(error)
       );
   }
--- src/app/rsvp/rsvp.service.ts
+++ src/app/rsvp/rsvp.service.ts
@@ -1,11 +1,34 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
+import 'rxjs/Rx';
 
 @Injectable()
 export class RsvpService {
   constructor(private http: Http){}
 
-  storeRsvp(rsvp: {}){
-    return this.http.post('https://dnb2017-d9f24.firebaseio.com/data.json', rsvp)
+  storeRsvp(rsvp: any[]){
+    return this.http.post('https://dnb2017-d9f24.firebaseio.com/data.json', rsvp);
+  }
+
+  getRsvp(firstName,lastName){
+    return this.http.get('https://dnb2017-d9f24.firebaseio.com/data.json')
+      .map(
+        (response: Response) => {
+          const data = response.json();
+          for (const rsvp of data){
+            for (const guest of rsvp.guests){
+              if(firstName.toLowerCase().replace(/[^A-Z0-9]/ig, "") == guest.firstName.toLowerCase() &&
+                lastName.toLowerCase().replace(/[^A-Z0-9]/ig, "") == guest.lastName.toLowerCase()){
+                return rsvp;
+              }
+            }
+          }
+          return;
+        }
+      );
+  }
+
+  populateOptions(){
+
   }
 }
