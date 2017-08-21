ffb139fcfa00c8606abbe2cb4766741e7ddca3ee
Kyle Valdillez <kyle.valdillez@gmail.com>
2017-08-21 13:17:58 -0700

    Updated RSVP component and mobile view

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
 	selector: 'app-header',
@@ -6,6 +6,46 @@ import { Component } from '@angular/core';
 	styleUrls: ['../app.component.css','./header.component.css']
 })
 
-export class HeaderComponent{
+export class HeaderComponent implements OnInit{
+
+	ngOnInit() {
+
+	}
+
+	toggleMobileNav() {
+		this.unfade(document.getElementById('menuOverlay'));
+		document.getElementById("mobileNav").style.width = "250px";
+	}
+
+	closeMenu() {
+		this.fade(document.getElementById('menuOverlay'));
+		document.getElementById("mobileNav").style.width = "0";
+	}
+
+	fade(element) {
+		var op = 1;  // initial opacity
+		var timer = setInterval(function () {
+			if (op <= 0.1){
+				clearInterval(timer);
+				element.style.display = 'none';
+			}
+			element.style.opacity = op;
+			element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+			op -= op * 0.1;
+		}, 5);
+  }
+
+	unfade(element) {
+		var op = 0.1;  // initial opacity
+		element.style.display = 'block';
+		var timer = setInterval(function () {
+			if (op >= .6){
+				clearInterval(timer);
+			}
+			element.style.opacity = op;
+			element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+			op += op * 0.1;
+		}, 5);
+	}
 
 }
