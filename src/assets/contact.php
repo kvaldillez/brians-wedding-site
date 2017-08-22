5b93b6d6d52b47103a68d79427c4aca16035eb8f
Kyle Valdillez <kyle.valdillez@gmail.com>
2017-08-22 13:40:57 -0700

    Updated mobile styles for RSVP and menu

---
+++ src/assets/contact.php
@@ -0,0 +1,32 @@
+<?php
+$field_name = $_POST['name'];
+$field_email = $_POST['email'];
+$field_message = $_POST['message'];
+
+$mail_to = 'kyle.valdillez@gmail.com';
+$subject = 'Message from KyleValdillez.com:'.$field_name;
+
+$body_message = 'From: '.$field_name."\n";
+$body_message .= 'E-mail: '.$field_email."\n";
+$body_message .= 'Message: '.$field_message;
+
+// $headers = 'From: '.$field_email."\r\n";
+$headers .= 'Reply-To: '.$field_email."\r\n";
+
+$mail_status = mail($mail_to, $subject, $body_message, $headers);
+
+if ($mail_status) { ?>
+	<script language="javascript" type="text/javascript">
+		alert('Thank you for the message. I will contact you as soon as possible.');
+		//window.location = '/contact';
+	</script>
+<?php
+}
+else { ?>
+	<script language="javascript" type="text/javascript">
+		alert('Message failed. Please, send an email to kyle.valdillez@gmail.com');
+		//window.location = '/contact';
+	</script>
+<?php
+}
+?>
